caec590dd0f5fe2d126f0dbee7645f51a5f12a64
Ajinkya S Khandave <ajinkyaakap11@gmail.com>
2020-05-19 14:49:58 +0530

    code

--- main.py
+++ main.py
@@ -1,60 +1,68 @@
+# Import essentials
+import keras
 from keras.datasets import mnist
+from keras.models import Sequential
+from keras.utils.np_utils import to_categorical
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.backend import clear_session
 import numpy
 
-def train_model(neurons) : 
+# Train model function
+
+def train_model(neurons , model , epochs , test) : 
+	print("\n" , " *** Summary *** " , "\n", "Iteration  : ", test , "\n" , "   Number of Neurons : ", neurons , "\n" , "   Number of Epochs : ",  epochs)
 	model.add(Dense(units = neurons , input_dim = 28*28 , activation = 'relu'))
-	model.summary()
 	model.add(Dense(units=200 , input_dim = 28*28 , activation = 'relu'))
-	model.summary()
 	model.add(Dense(units=60 , input_dim = 28*28 , activation = 'relu'))
-	model.summary()
 	model.add(Dense(units=10 , input_dim = 28*28 , activation = 'softmax'))
-	model.summary()
-	from keras.optimizers import Adam
 	model.compile( optimizer= "Adam" , loss='categorical_crossentropy', 
 	             metrics=['accuracy'] )
-	fit_model = model.fit(train_X ,  train_y , epochs = 3)
-	return fit_model
+	return model
 
-def validate(fit_model):
+
+def validate(fit_model, epochs):
 	text = fit_model.history
-	accuracy = text['accuracy'][2]
-	accuracy = accuracy * 100
+	accuracy = text['accuracy'][epochs-1] * 100
 	accuracy = int(accuracy)
-	accuracy = str(accuracy)
 	f= open("accuracy.txt","w+")
-	f.write(accuracy)
+	f.write(str(accuracy))
 	f.close()
-	print(accuracy)
+	print("    Accuracy for this Iteration is : " , accuracy ,"%")
 	return accuracy
 
-dataset = mnist.load_data("mymnist.data")
-train, test = dataset
-train_X , train_y = train
-test_X , test_y = test
-import matplotlib.pyplot as plt
+
+
+# Load Model 
+(train_X , train_y), (test_X , test_y) = mnist.load_data("mymnist.data")
+# Reshape data and change type
 test_X = test_X.reshape(-1 , 28*28)
 train_X = train_X.reshape(-1 ,  28*28)
 test_X = test_X.astype("float32")
 train_X = train_X.astype("float32")
-from keras.utils.np_utils import to_categorical
+# One hot encoding 
 test_y = to_categorical(test_y)
 train_y = to_categorical(train_y)
-from keras.models import Sequential
-model = Sequential()
+
+
+#Initials
 neurons = 10
-from keras.layers import Dense
-fit_model=train_model(neurons)
-accuracy = validate(fit_model)
+accuracy = 0
+epochs = 1
+test = 1
+flag = 0
 
-
-while int(accuracy) < 90 :
-	print("Model trained successfully but accuracy is less than 95%")
-	fit_model=train_model(neurons+10)
-	accuracy=validate(fit_model)
-
-print("Code run successfully ")
-
-
-
+while int(accuracy) < 96 :
+	if flag == 1 :
+		model = keras.backend.clear_session()
+		neurons = neurons+10
+		epochs = epochs+1 
+		test = test + 1
+	#model=reset_weights(model)
+	model = Sequential()
+	model = train_model(neurons , model , epochs , test)
+	print("    calculating accuracy . . .")
+	fit_model = model.fit(train_X ,  train_y , epochs = epochs , verbose =  False)
+	accuracy=validate(fit_model , epochs)
+	flag = 1
 
